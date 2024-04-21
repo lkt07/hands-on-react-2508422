043d93feeb02f58904bfa2cf8b52666132e7ba63
Lakshmikanth <49640964+lkt07@users.noreply.github.com>
2024-04-21 16:23:35 +0000

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
       <div className="container">
         <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
+          <img src="images/group.svg" alt="StarGazers Grp" />
           <h1>Meet the Stargazers</h1>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
